72355774b4ba45a935f1b51a41a63b98c808ddfd
kacper <kacperkrajewski52@gmail.com>
2025-05-08 22:57:03 +0200

    build: project init

---
+++ src/main/java/com/example/demo_task2/DemoTask2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTask2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_task2/DemoTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTask2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
